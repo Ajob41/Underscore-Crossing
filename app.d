7305a556d60b5f9d57ef7d9e48bc83bdbf65354d
Ajob41 <jobwrsme41@gmail.com>
2024-08-04 07:59:01 -0500

    first commit

---
+++ app.d
@@ -0,0 +1,49 @@
+import core.stdc.stdio;
+import core.stdc.stdlib;
+
+extern(C) void main() {
+    // Allocate memory for 2 integers
+   int number = 123;
+   printf("%d",isNegative(number));
+}
+
+int toDecimal(int number) {
+	if(number > 0) {
+		number = -number;
+	}
+	return number;
+}
+
+int toPositive(int number) {
+	if(number < 0) {
+		number = -number;
+	}
+	return number;
+}
+
+bool isPositive(int number) {
+	if(number > 0) {
+		return true;
+	}
+	return false;
+}
+
+bool isNegative(int number) {
+	if(isPositive(number) == 1) {
+		return false;
+	}
+	return true;
+}
+
+// void toString(int number) {
+// 	 if(number < 0) {
+// 		number = toPositive(number);
+// 	 }
+// 	 char[]finalResult;
+// 	 while(number > 0) {
+// 		int result = number%10;
+// 		char resultToChar = cast(char)(result+'0');
+// 		number = number/10;
+// 	 }
+// }
+
