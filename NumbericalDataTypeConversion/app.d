237d9636fdfa1e7d34d21e693c66ef0cc8bcd591
Ajob41 <jobwrsme41@gmail.com>
2024-08-16 08:54:23 -0500

    added typeOfNumber function

--- NumbericalDataTypeConversion/app.d
+++ NumbericalDataTypeConversion/app.d
@@ -3,37 +3,91 @@ module NumbericalDataTypeConversion.app;
 import core.stdc.stdio;
 import core.stdc.stdlib;
 
-extern(C) void main() {
-    // Allocate memory for 2 integers
-   float number = 123.0;
-   printf("%d",number);
+extern (C) void main()
+{
+
+	bool negative = isNegative(-12);
+	printf("%d", negative);
+
 }
 
-int toNegative(int number) {
-	if(number > 0) {
+int toNegative(int number)
+{
+	if (number > 0)
+	{
 		number = -number;
 	}
 	return number;
 }
 
-int toPositive(int number) {
+int toPositive(int number)
+{
 	int result = -toNegative(number);
 	return result;
 }
 
-bool isPositive(int number) {
-	if(number > 0) {
+bool isPositive(int number)
+{
+	if (number > 0)
+	{
+
 		return true;
 	}
-	return false;
+	else
+	{
+
+		return false;
+
+	}
+
 }
 
-bool isNegative(int number) {
-	if(isPositive(number) == 1) {
+bool isNegative(int number)
+{
+	if (!isPositive(number))
+	{
+
+		return true;
+	}
+	else
+	{
+
 		return false;
 	}
-	return true;
+
 }
 
 
+string typeOfNumber(T)(T t) @nogc
+{   
+    
+    if (t - cast(int) t == 0)
+    {
+        return "whole";
+    }
+    else if (t - cast(uint) t == 0)
+    {
+        return "whole";
+    }
+    else if (t - cast(long) t == 0)
+    {
+        return "whole";
+    }
+    else if (t - cast(ulong) t == 0)
+    {
+        return "whole";
+    }
+    else if (t - cast(short) t == 0)
+    {
+        return "whole";
+    }
+    else if (t - cast(ushort) t == 0)
+    {
+        return "whole";
+    }
+    else
+    {
+        return "decimal";
+    }
 
+}
